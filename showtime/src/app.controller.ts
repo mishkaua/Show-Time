dee0dc9c2e486c5215c45209654403847153bc8f
Razmo <you@example.com>
2023-12-13 11:38:47 +0100

    app controller

--- showtime/src/app.controller.ts
+++ showtime/src/app.controller.ts
@@ -7,7 +7,7 @@ import { AuthService } from './auth/auth.service';
 export class AppController {
   constructor(private authService: AuthService) {}
 
-  /*  @UseGuards(LocalAuthGuard)
+ /*  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
